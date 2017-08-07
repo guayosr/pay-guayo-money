764b94005bd95ba4963a67b74c678d20e7348d28
Eduardo Serrano <eduardo@stripe.com>
2017-08-06 20:53:40 -0700

    first commit

---
+++ app.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+require 'stripe'
+ 
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
+
+get '/' do
+  erb :index
+end
+
+get '/test' do
+  erb :test
+end
+
+post '/charge' do
+  source = params[:source]
+
+  begin 
+    charge = Stripe::Charge.create(
+      :amount => params[:amount],
+      :currency => "usd",
+      :description => "Example charge",
+      :source => source,
+    )
+    erb :success
+
+  rescue Stripe::CardError => e
+    body = e.json_body
+    err  = body[:error]
+    @error = err[:message]
+
+    erb :index
+  end
+end
